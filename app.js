503285a1ad1a2301696b5eac1a3c12ca6a07ac46
github username <github email address>
2018-07-26 23:47:19 -0400

    added ejs

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ var port = process.env.PORT || 3000;
 
 app.use("/assets", express.static(__dirname + "/public"));
 
-app.get('/', function (request, response){
-  response.send("<html><head><body><h1>Hello World!!!</h1></body></head></html>")
+app.set('view engine', 'ejs');
+
+// app.get('/', function (request, response){
+//   response.send("<html><head><body><h1>Hello World!!!</h1></body></head></html>")
+// });
+
+app.get('/', function(req, res) {
+	res.render('index');
 });
 
 app.get('/middleware', function (request, response){
   response.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello with middleware!!!</h1></body></html>')
 });
 
-app.get('/person/:id', function(request, response){
-  response.send("<html><head><body><h1>Person: " + request.params.id + "</h1></body></head></html>")
-})
+// app.get('/person/:id', function(request, response){
+//   response.send("<html><head><body><h1>Person: " + request.params.id + "</h1></body></head></html>")
+// })
+
+app.get('/person/:id', function(req, res) {
+	res.render('person', { ID: req.params.id });
+});
 
 app.get('/api', function(request, response){
   response.json({
